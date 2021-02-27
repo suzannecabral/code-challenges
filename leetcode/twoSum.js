aa5480c535f8bf09279b06b32417b7d15df978a8
Suzanne Cabral <suzannecabral@live.com>
2021-02-26 18:28:44 -0800

    Solved Leetcode Two-Sum

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -43,15 +43,154 @@ Constraints:
  * @return {number[]}
  */
 const twoSum = function(nums, target) {
-    let ans = "answer";
+    let ans = [];
+
+
+    for(thisNum = 0; thisNum < nums.length; thisNum++){
+        for(nextNum = thisNum+1; nextNum < nums.length; nextNum++){
+            if(target - nums[thisNum] == nums[nextNum]){
+                ans.push(thisNum, nextNum)
+                break;
+            }else{
+                continue;
+            }
+        }
+    }
+
 
     console.log(ans);
     return(ans);
 };
 
-twoSum([2,7,11,15],9);
+// twoSum([2,7,11,15],9);
+// twoSum([3,2,4],6);
+twoSum([3,3],6);
 
-/* TODO: write notes about challenges */
+
+
+/* 
+-----------------
+The Brute Force Ugly Way
+-----------------
+return array: indices of the 2 nums that add up to the target
+target - thisNum = the other number
+
+
+Pseudocode
+-----------------
+let lastChecked = -1
+
+for each input(thisNum) (loop)
+    lastChecked += 1
+    if target - thisNum == arr[lastChecked+1] 
+        ! it's the answer, return it
+        
+        break    
+
+    else keep checking
+
+
+Code Try 1
+-------------------
+let lastChecked = -1
+for (i=0; i<nums.length; i++){
+    const thisNum = arr[i];
+    const nextNum = arr[i+1]
+    lastChecked += 1;
+
+    if(target-thisNum == nextNum){
+        >> this is the answer return it
+        >> form the answer array
+    }else{
+        >> keep going thru array
+        >> this sounds like a nested loop, how do I write nested loops?
+        >> the ideal solution avoids nested loops. Good thing this is the ugly solution and not the ideal one.
+
+    }
+}
+
+
+Code Try 2
+--------------------
+Writing the nested loop
+
+let lastChecked = -1
+for (i=0; i<nums.length; i++;){
+    const thisNum = arr[i];
+    const nextNum = arr[i+1]
+    lastChecked += 1;
+
+    if(target-thisNum == nextNum){
+        >> this is the answer return it
+        >> form the answer array
+    }else{
+        for (j=i+1; j<nums.length; j++;){
+            nextNum = arr[j];
+            if(target-thisNum = nextNum)
+        }
+    }
+}
+
+
+Code Try 3
+--------------------
+I don't need that first conditional statement
+I can roll the variable names into the loop statement
+spaces around operators helps with reading them
+I should really call my variables thisIdx and nextIdx but too late now
+
+for(thisNum = 0; thisNum < nums.length; thisNum++;){
+    for(nextNum = thisNum+1; nextNum < nums.length; nextNum++;){
+        if(target - arr[thisNum] == arr[nextNum]){
+            ans.push(thisNum, nextNum)
+            break;
+        }else{
+            continue;
+        }
+    }
+}
+
+
+Testing Code from Try 3
+--------------------
+deleted the semicolons
+change arr to nums
+all 3 passed! on to Leetcode
+
+
+Leetcode Stats
+--------------------
+Runtime: 76 ms, faster than 83.53% of JavaScript online submissions for Two Sum.
+Memory Usage: 39.1 MB, less than 20.71% of JavaScript online submissions for Two Sum
+- how is this faster than 83%??? 
+- Space improvement: I can remove the separate answer array and return from inside the conditional, which would also not need to break
+- improved version: one-pass hash table, can go through the list only once. 
+
+
+Notes
+--------------------
+The extra variables are less efficient, but help me read. My workflow is stopping and starting, need the help picking the code back up
+I miss python
+
+
+Things I Googled 
+-----------------
+    js break
+        used on its own line to break out of a loop
+    js array length
+        syntax: arr.length
+        also loop syntax w/o array methods
+    js increment operator
+    js +=
+        yes you can use it in js
+    js does i++ need a semicolon
+        explainer: https://www.codecademy.com/forum_questions/507f6dd09266b70200000d7e#508000efde2d860200004bbe
+        it's optional before the newline
+        it shouldn't break if I have it (I wrote it with ;)
+        - nevermind, linter said to remove it
+    js continue
+        I don't think I need it, I could just write the if with no else. I think I'll use it anyway in case I need console logs after or something.
+*/
 
 
 
